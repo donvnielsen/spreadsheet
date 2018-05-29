28d421d1b5aae046bb76ca9ed04e7a767d526b82
donvnielsen <donvnielsen@gmail.com>
2018-05-29 17:02:51 -0500

    added spreadsheet specific logic

--- process_spreadsheet.rb
+++ process_spreadsheet.rb
@@ -1,15 +1,28 @@
 require 'roo'
+require 'pp'
 
-workbook = Roo::Spreadsheet.open './sample_excel_files/xlsx_500_rows.xlsx'
+TBLNAME = 'tbCMDBApplication'
+DEVELOPERS = 9..10
+
+def create_sql_update(cols)
+  [
+     "UPDATE #{TBLNAME} ",
+     "SET #{} = #{}, #{} = #{}",
+     "WHERE id = #{}"
+  ].join('\n')
+end
+
+workbook = Roo::Spreadsheet.open './data/cmdb_master_v3.xlsx'
 worksheets = workbook.sheets
 puts "Found #{worksheets.count} worksheets"
 
-worksheets.each do |worksheet|
-  puts "Reading: #{worksheet}"
-  num_rows = 0
-  workbook.sheet(worksheet).each_row_streaming do |row|
-    row_cells = row.map { |cell| cell.value }
-    num_rows += 1
-  end
-  puts "Read #{num_rows} rows"
+pp worksheets
+
+num_rows = 0
+workbook.sheet(TBLNAME).each_row_streaming do |row|
+  pp row;break
+  # row_cells = row.map { |cell| cell.value }
+  # pp row_cells
+  # num_rows += 1
 end
+puts "Read #{num_rows} rows"
