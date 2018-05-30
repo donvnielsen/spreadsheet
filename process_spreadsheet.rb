7e1acec15118f553a553c4a9ed3c5cd80a6123eb
dvn <donvnielsen@gmail.com>
2018-05-30 06:15:50 -0500

    Finish logic

--- process_spreadsheet.rb
+++ process_spreadsheet.rb
@@ -2,27 +2,42 @@ require 'roo'
 require 'pp'
 
 TBLNAME = 'tbCMDBApplication'
-DEVELOPERS = 9..10
+PRIMARY = 8
+SECONDARY = 9
 
-def create_sql_update(cols)
-  [
-     "UPDATE #{TBLNAME} ",
-     "SET #{} = #{}, #{} = #{}",
-     "WHERE id = #{}"
-  ].join('\n')
+def create_sql_update(cols,id)
+  ary = ["UPDATE #{TBLNAME}"]
+  i = 0
+  cols.each {|k,v|
+    ary << "#{(i+=1) == 1 ? 'SET ' : ', '}#{k} = '#{v}'"
+  }
+  ary << "WHERE id = '#{id}';"
+  ary.join(' ')
 end
 
 workbook = Roo::Spreadsheet.open './data/cmdb_master_v3.xlsx'
 worksheets = workbook.sheets
-puts "Found #{worksheets.count} worksheets"
-
-pp worksheets
+puts "Found #{worksheets.count} worksheets:"
+puts worksheets,'-'*70
 
 num_rows = 0
+pri = nil
+sec = nil
 workbook.sheet(TBLNAME).each_row_streaming do |row|
-  pp row;break
-  # row_cells = row.map { |cell| cell.value }
-  # pp row_cells
-  # num_rows += 1
+  row_cells = row.map { |cell| cell.value }
+
+  if num_rows == 0
+    # pp row_cells;break
+    pri = row_cells[PRIMARY]
+    sec = row_cells[SECONDARY]
+  else
+    cols = {}
+    cols[pri] = row_cells[PRIMARY].nil? ? '' : row_cells[PRIMARY]
+    cols[sec] = row_cells[SECONDARY].nil? ? '' : row_cells[SECONDARY]
+    id = /\{(?<guid>[A-Za-z0-9-]*)\}/.match(row_cells[0])
+    puts create_sql_update(cols,id[:guid]) unless num_rows == 0
+  end
+  num_rows += 1
+
 end
-puts "Read #{num_rows} rows"
+puts '-'*70,"Read #{num_rows} rows"
